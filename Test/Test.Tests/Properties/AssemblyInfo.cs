c387337f39f95d51a6831c1b7d13aa92d2d6569d
444cilya444 <55949092+444cilya444@users.noreply.github.com>
2022-09-21 17:18:51 +0300

    Тестовое задание

---
+++ Test/Test.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("92a6c392-bf16-47e2-a3fb-bff3dde67918")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Test/TestMyLib/Interface.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TestMyLib
+{
+    internal interface IFigure
+    {
+        string Sides(double[] sides);
+    }
+    public class CircleSquare : IFigure
+    {
+        public string Sides(double[] sides)
+        {
+            return "Круг " + Math.Round(Math.PI * Math.Pow(sides[0], 2), 2);
+        }
+    }
+    public class TriangleSquare : IFigure
+    {
+        double[] straightTriangle;
+        public string Sides(double[] sides)
+        {
+            straightTriangle = sides;
+            double p = (sides[0] + sides[1] + sides[2]) / 2;
+            double square = Math.Sqrt(p * (p - sides[0]) * (p - sides[1]) * (p - sides[2]));
+            return "Треугольник " + square;
+        }
+        public bool СorrectnessTriangle()
+        {
+            bool isStraight = (straightTriangle[0] == Math.Sqrt(Math.Pow(straightTriangle[1], 2) + Math.Pow(straightTriangle[2], 2))
+                || straightTriangle[0] == Math.Sqrt(Math.Pow(straightTriangle[1], 2) + Math.Pow(straightTriangle[2], 2))
+                || straightTriangle[0] == Math.Sqrt(Math.Pow(straightTriangle[1], 2) + Math.Pow(straightTriangle[2], 2)));
+            return isStraight;
+        }
+        public string TriangleIsRectangular()
+        {
+            if (straightTriangle[0] < 0 || straightTriangle[1] < 0 || straightTriangle[2] < 0)
+            {
+                return "Такого треугольника не существует(сторона(ы) меньше 0)";
+            }
+
+            if (straightTriangle[0] > (straightTriangle[1] + straightTriangle[2]) || straightTriangle[1] > (straightTriangle[0] + straightTriangle[2]) || straightTriangle[2] > (straightTriangle[0] + straightTriangle[1]))
+            {
+                return "Такого треугольника не существует(сторона больше суммы двух других)";
+            }
+            return "Tреугольник существует";
+        }
+    }
+}
