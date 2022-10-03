e9a595cef31c252f9b96bbd1fc7ad75001a51524
444cilya444 <55949092+444cilya444@users.noreply.github.com>
2022-10-03 18:57:36 +0300

    Up

---
+++ Задание с сортировкой массива/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arry = { 3, 12, 4, 10 };
+            var orderedArry = arry.OrderBy(n => n);
+            foreach (int i in orderedArry)
+                Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
